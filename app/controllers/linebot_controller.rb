b15ba9d7181e02074ac56f8ae45dc96a9e56e0b0
西尾祥希 <nishio@nishiosachinozominoMacBook-Pro.local>
2020-05-12 11:55:31 +0900

    実験

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,6 +1,13 @@
 class LinebotController < ApplicationController
   require 'line/bot'  # gem 'line-bot-api'
 
+    #webに接続するためのライブラリ
+    require "open-uri"
+    #クレイピングに使用するライブラリ
+    require "nokogiri"
+
+    require 'csv'
+
   # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
 
@@ -21,12 +28,6 @@ class LinebotController < ApplicationController
 
     #client.reply_message(event['replyToken'], scraping)
     def scraping
-      #webに接続するためのライブラリ
-      require "open-uri"
-      #クレイピングに使用するライブラリ
-      require "nokogiri"
-
-      require 'csv'
 
       #クレイピング対象のURL
       url = "https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-%E3%83%AA%E3%83%B3%E3%82%B0%E3%83%95%E3%82%A3%E3%83%83%E3%83%88-%E3%82%A2%E3%83%89%E3%83%99%E3%83%B3%E3%83%81%E3%83%A3%E3%83%BC-Switch/dp/B07XV8VSZT/ref=sr_1_1?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&dchild=1&keywords=%E3%83%AA%E3%83%B3%E3%82%B0%E3%83%95%E3%82%A3%E3%83%83%E3%83%88%E3%82%A2%E3%83%89%E3%83%99%E3%83%B3%E3%83%81%E3%83%A3%E3%83%BC&qid=1588166354&s=videogames&sr=1-1"
@@ -58,11 +59,11 @@ class LinebotController < ApplicationController
         result = result.delete("￥")
         result = result.delete(",")
 
-         return result
+         return result.encode("sjis")
          sleep(60)
     end
 
-    events = scraping()
+    events = client.parse_events_from(body)
 
     events.each { |event|
       case event
@@ -71,7 +72,7 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           message = {
             type: 'text',
-            text: event.message['text']
+            text: scraping()
           }
           client.reply_message(event['replyToken'], message)
         end
